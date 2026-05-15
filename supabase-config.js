40dc9b9b7c82e7f39df5b3db510bd30eab91b2f8
Artur Mustata <artur23bike@gmail.com>
2026-05-15 03:50:30 +0300

    Publish employee tracker

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL = "https://wvskmmbvekvrrwzjvons.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_doAutrarS7tHU_OR1GZJ-w_nnKQCPbv";
+
+const OWNER_PIN = "1234";
+
+const DEFAULT_EMPLOYEE_CODE = "EMP-001";
